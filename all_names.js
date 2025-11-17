b27bbc0d41e1974820a433031ccf739dc95c8d2d
Kambiré Cyé Antonio Angelberg <coding@DNa1d6392.SUNet>
2025-11-16 21:23:17 -0800

    Ran find top 1000 process

--- all_names.js
+++ all_names.js
@@ -3,7 +3,8 @@ import path from 'path';
 import { execSync } from 'child_process';
 import { debug_mode, download_count_start, download_count_end } from './config.js'
 import { db } from './database.js'
-
+import names from "all-the-package-names" assert { type: 'json' };
+import fs from 'fs'
 
 const threshold = 10000; // minimum downloads to consider
 
@@ -46,21 +47,54 @@ async function fetchDownloadsWithRetries(package_name, options = {}, retries = 3
             }
         }
     }
+    return null;
 }
 
 export async function count_downloads(package_name){
     // TODO: Put your own end and start data depending on the number agreed for research
     // Remember that you do not want to include the dates after which you started tinkering with this (post October 9th)
     let data = await fetchDownloadsWithRetries(package_name);
+    if(!data){
+        return null;
+    }
     data = await data.json();
     let count = 0;
     if(data && "downloads" in data){
         for(let i = 0; i < data["downloads"].length; i++){
-            count += data["downloads"][i]["downloads"];
+            count += parseInt(data["downloads"][i]["downloads"]);
         }
     }
     return count;
 }
+
+// the following will find the top 1000 packages downlaoded in the past 6 months and will write it to file_name
+export async function find_top_1000(file_name, start = 0, end = names.length) {
+  const chosen = [];
+
+  for (let i = start; i < end; i++) {
+    if(i != 0 && i % 50000 == 0){
+        console.log(`Done up to ${i} at ${new Date().toLocaleString()}`);
+        fs.writeFileSync(`./output_files/partial_top_1000_${i}.json`, JSON.stringify(chosen, null, 2));
+    }
+    const name = names[i];
+    const back_s = name.indexOf("/");
+
+    const count = parseInt(await count_downloads(name), 10);
+    if (!count) continue;
+
+    if (chosen.length < 1000) {
+      chosen.push([count, name]);
+      chosen.sort((a, b) => a[0] - b[0]);
+    } else if (count > chosen[0][0]) {
+      chosen[0] = [count, name];
+      chosen.sort((a, b) => a[0] - b[0]);
+    }
+  }
+  fs.writeFileSync(file_name, JSON.stringify(chosen, null, 2));
+}
+console.log(`Started process at ${new Date().toLocaleString()} with a total of ${names.length} packages in npm`);
+await find_top_1000("./output_files/top1000.json", 0, names.length);
+
 
 // given an npm package name,
 // (1) find the github repo corresponding to it
--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 export const debug_mode = false;
 export const phantom_file_path = 'phantom.txt';
-export const download_count_start = '2025-09-01';
-export const download_count_end = '2025-10-01';
+export const download_count_start = '2025-05-11';
+export const download_count_end = '2025-11-11';
 export const auto_range_start = '1000000';
 export const auto_range_end = '1000100';
